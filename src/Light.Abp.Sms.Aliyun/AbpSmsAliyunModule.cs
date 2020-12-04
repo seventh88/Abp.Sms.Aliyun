4ff1a932878493e25b9d3357ddd1484bf85e3988
seventh88 <5856868x@gmail.com>
2020-12-04 14:48:54 +0800

    remove defaut Configure

--- src/Light.Abp.Sms.Aliyun/AbpSmsAliyunModule.cs
+++ src/Light.Abp.Sms.Aliyun/AbpSmsAliyunModule.cs
@@ -8,13 +8,6 @@ namespace Light.Abp.Sms.Aliyun
 
         public override void ConfigureServices(ServiceConfigurationContext context)
         {
-            var configuration = context.Services.GetConfiguration();
-            Configure<AbpSmsAliyunOption>(options =>
-            {
-                options.RegionId = configuration["Ali:RegionId"];
-                options.AccessKey = configuration["Ali:AccessKey"];
-                options.AccessKeySecret = configuration["Ali:AccessKeySecret"];
-            });
         }
     }
 }
